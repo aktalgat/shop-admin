ea1f5a0ec675189dc0bd2caae8979c77b683ed15
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2017-02-25 11:29:14 +0600

    Add login service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { AppService } from './../app.service'
+import { AppService } from '../app.service'
 import { Router } from '@angular/router'
+import { LoginService } from './login.service'
 
 @Component({
   selector: 'login',
   templateUrl: 'login.component.html',
-  styleUrls: ['login.component.css']
+  styleUrls: ['login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent {
   constructor (private appService: AppService, private router: Router) {
