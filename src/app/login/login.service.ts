18b4fabc881a59ccfd4afc58d700484a7fd48b59
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2017-02-27 12:14:27 +0600

    Add handle success and catch error

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from "@angular/core";
-import { Headers, Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { Headers, RequestOptions, Response, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -7,5 +10,26 @@ export class LoginService {
 
   private loginUrl = 'api/login';
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
+
+  getLoggedIn(): Observable<any> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.loginUrl, {}, options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  extractData(response: Response) {
+    let body = response.json();
+    console.log(body);
+    return body.data || { };
+  }
+
+  handleError(error: Response | any) {
+    let errMsg: string;
+
+    return Observable.throw(errMsg);
+  }
 }
