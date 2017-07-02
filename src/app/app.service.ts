3e2d93af153cf52a48c4755db66056b90f0c5ca9
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2017-07-02 18:54:39 +0600

    Start load json config

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,19 @@
 import {Injectable} from "@angular/core";
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import {Http, Response} from "@angular/http";
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {environment} from '../environments/environment';
 
 @Injectable()
 export class AppService {
+
+  apiUrl = environment.apiUrl;
+
+  constructor (private http: Http) {}
+
+  getStructure() {
+    return this.http.get(this.apiUrl  + '');
+  }
+
   public showMenuSource = new BehaviorSubject<boolean>(true);
 
   public setShowMenu(showMenu: boolean) {
